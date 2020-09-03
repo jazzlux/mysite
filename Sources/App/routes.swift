8a40aaf4af1fc115ae95491f6ec316df3281a37d
Piotr <piotr@fastmail.co.uk>
2020-09-03 18:16:32 +0100

    context test

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,6 +3,41 @@ import Vapor
 import Leaf
 
 func routes(_ app: Application) throws {
+    
+//    struct Item: Codable {
+//        var title: String
+//        var description: String
+//    }
+//
+//
+//    app.get("users") { req -> EventLoopFuture<View> in
+//
+//        struct Context: Codable {
+//            let items: [Item]
+//        }
+//
+//        let context = Context(items: [
+//            .init(title: "#01", description: "Description #01"),
+//            .init(title: "#02", description: "Description #02"),
+//            .init(title: "#03", description: "Description #03"),
+//
+//        ])
+//
+//
+//        return req.view.render("index", context)
+//    }
+    
+    
+    app.get("text") { req in
+         req.view.render("text", [
+             "title": "o mnie",
+             "description": "This is my own page.",
+             "content": "dziele sie o mnie..."
+         ])
+     }
+     
+    
+    
     app.get { req in
         return "It works!"
     }
@@ -11,13 +46,13 @@ func routes(_ app: Application) throws {
         return "Hello, world!"
     }
     
-    app.get("index") { req in
-        req.view.render("index", [
-            "title": "Hi",
-            "body": "Hello world!",
-            "ilosc": "3"
-        ])
-    }
+//    app.get("index") { req in
+//        req.view.render("index", [
+//            "title": "Hi",
+//            "body": "Hello world!",
+//            "ilosc": "3"
+//        ])
+//    }
 
     try app.register(collection: TodoController())
 }
